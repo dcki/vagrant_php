a2d9be34ce41541efae52c74c2d3a4a5f465044e
david <david.winiecki@gmail.com>
2015-01-10 18:39:57 -0800

    Work on routing in config.php.

--- www/php/config.php
+++ www/php/config.php
@@ -1,5 +1,58 @@
 <?php
 
-include_once 'index.php';
+// Get all needed info from request.
+
+$url = $_SERVER['REQUEST_URI'] ? $_SERVER['REQUEST_URI'] : $_SERVER['REDIRECT_URL'];
+
+
+
+
+
+// Global variables often cause issues and they're unnecessary. Delete them.
+
+// Display all globals.
+//var_export(array_keys($GLOBALS));
+
+$GLOBALS  = null;
+$_SERVER  = null;
+$_GET     = null;
+$_POST    = null;
+$_FILES   = null;
+$_COOKIE  = null;
+$_SESSION = null;
+$_REQUEST = null;
+$_ENV     = null;
+
+
+
+
+
+// Register autoloader.
+
+
+
+
+
+// Require utility functions.
+
+//require_once '';
+
+
+
+
+
+// Route.
+
+if (preg_match('/^(\/)?$/', $url)) {
+   
+   require_once 'index.php';
+
+} else if (preg_match('/abc/', $url)) {
+
+
+} else {
+   
+   require_once '../404.html';
+}
 
 ?>
